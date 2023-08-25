da2da01404ed6d378895828d4b00499a9d4624b0
ankurdev002 <contact.dev.ankur@gmail.com>
2023-08-26 00:22:00 +0530

    first commit

---
+++ Star Pattern/Star Pattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Star_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int patternHeight = 4;
+
+            for(int i = 1; i <= patternHeight; i++)
+            {
+                //print the spaces of stars
+                for(int j = 1; j <= patternHeight - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                //this loop will print star after the space
+                for(int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                //below console.writeline is used to move to the next line and print other row stars
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Valid date from string/Valid date from string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_date_from_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hdjsh asd2324234jghjsd hjsdg sdhk 12212021 idf32432 32423 d34234jh dfh";
+            string[] words = input.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (word.Length == 8 && int.TryParse(word, out _))
+                {
+                    Console.WriteLine(word);
+                    break;
+                }
+            }
+
+        }
+    }
+}
